9f4bab523033395a6c9f2413c6813aa21246a65d
Akshat Sharma <akshat.sharma@grazitti.com>
2025-05-19 14:42:40 +0530

    added string enhancement

--- src/string-utils.py
+++ src/string-utils.py
@@ -4,3 +4,7 @@ def reverse_string(s):
 
 def capitalize_words(s):
     return ' '.join(word.capitalize() for word in s.split())
+
+def is_palindrome(s):
+    s_clean = ''.join(c.lower() for c in s if c.isalnum())
+    return s_clean == s_clean[::-1]
